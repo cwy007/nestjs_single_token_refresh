ca03d2f9adb1e0c73a9e02ad43e1c30c3bb62bc6
chanweiyan <chanweiyan007@gmail.com>
2026-05-08 01:37:04 +0800

    fix: generate jwt when user login

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    JwtModule.register({
+      global: true, // Make the JwtModule available globally
+      secret: 'your_jwt_secret_key', // In production, use a secure environment variable
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,36 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Inject,
+  BadRequestException,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { LoginUserDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
+  @Inject(JwtService)
+  private readonly jwtService: JwtService;
+
   @Post('login')
   async login(@Body() loginUserDto: LoginUserDto) {
+    if (
+      loginUserDto.username !== 'admin' ||
+      loginUserDto.password !== 'password'
+    ) {
+      throw new BadRequestException('username or password is incorrect');
+    }
+
+    const token = this.jwtService.sign(
+      { username: loginUserDto.username },
+      { expiresIn: '7d' },
+    );
+
     console.log('LoginUserDto:', loginUserDto);
-    return this.userService.login(loginUserDto);
+    return token;
   }
 }
