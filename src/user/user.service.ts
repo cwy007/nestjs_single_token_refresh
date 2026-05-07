24fd0672911af7e4092573364f9676d3485afef5
chanweiyan <chanweiyan007@gmail.com>
2026-05-08 01:31:19 +0800

    fix: add /user/login

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async login(loginUserDto: LoginUserDto) {
+    // Here you would typically validate the user's credentials and generate a token
+    // For demonstration purposes, we'll just return the loginUserDto
+    return {
+      message: 'Login successful',
+      user: loginUserDto,
+    };
   }
 }
