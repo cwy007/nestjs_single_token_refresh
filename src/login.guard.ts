5dd4646338ca68a4879c8f77ecef2b57321bdd6e
chanweiyan <chanweiyan007@gmail.com>
2026-05-08 01:46:55 +0800

    fix: when request set new token in the repsonse header for the client to use in subsequent requests

--- src/login.guard.ts
+++ src/login.guard.ts
@@ -1,11 +1,16 @@
-import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class LoginGuard implements CanActivate {
-
   @Inject(JwtService)
   private readonly jwtService: JwtService;
 
@@ -13,6 +18,7 @@ export class LoginGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request: Request = context.switchToHttp().getRequest();
+    const response: Response = context.switchToHttp().getResponse();
     const authHeader = request.headers['authorization'];
     if (!authHeader) {
       throw new UnauthorizedException('user is not logged in');
@@ -22,6 +28,12 @@ export class LoginGuard implements CanActivate {
     try {
       const decoded = this.jwtService.verify(token);
       request['user'] = decoded; // Attach decoded user info to the request object
+
+      const refreshToken = this.jwtService.sign(
+        { username: decoded.username },
+        { expiresIn: '7d' },
+      ); // Generate a new token with a longer expiration time
+      response.setHeader('token', refreshToken); // Set the token in the response header for the client to use in subsequent requests
       return true;
     } catch (err) {
       throw new UnauthorizedException('invalid token, please log in again');
