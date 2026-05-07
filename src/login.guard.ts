9aa56a9de9e9c2e0ef77df3262dd9bdf9778c319
chanweiyan <chanweiyan007@gmail.com>
2026-05-08 02:15:03 +0800

    fix: If the token expires in less than 1 day (86400 seconds), generate a new one

--- src/login.guard.ts
+++ src/login.guard.ts
@@ -29,11 +29,16 @@ export class LoginGuard implements CanActivate {
       const decoded = this.jwtService.verify(token);
       request['user'] = decoded; // Attach decoded user info to the request object
 
-      const refreshToken = this.jwtService.sign(
-        { username: decoded.username },
-        { expiresIn: '7d' },
-      ); // Generate a new token with a longer expiration time
-      response.setHeader('token', refreshToken); // Set the token in the response header for the client to use in subsequent requests
+      // If the token expires in less than 1 day (86400 seconds), generate a new one
+      const now = Math.floor(Date.now() / 1000);
+      if (decoded.exp && decoded.exp - now < 24 * 60 * 60) {
+        const newToken = this.jwtService.sign(
+          { username: decoded.username },
+          { expiresIn: '7d' },
+        ); // Generate a new token with a longer expiration time
+        response.setHeader('token', newToken); // Set the token in the response header for the client to use in subsequent requests
+      }
+
       return true;
     } catch (err) {
       throw new UnauthorizedException('invalid token, please log in again');
